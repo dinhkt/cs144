115c7643d1047b12b057f236097dd373ef55078d
dinhkt <dinhkhactuyen97@gmail.com>
2020-03-24 17:17:28 +0700

    handle simple arp

--- lab3/router/sr_router.c
+++ lab3/router/sr_router.c
@@ -13,7 +13,8 @@
 
 #include <stdio.h>
 #include <assert.h>
-
+#include <stdlib.h>
+#include <string.h>
 
 #include "sr_if.h"
 #include "sr_rt.h"
@@ -29,7 +30,9 @@
  * Initialize the routing subsystem
  *
  *---------------------------------------------------------------------*/
-
+void reply_arp(struct sr_instance *sr, void *arp_packet,unsigned int len,struct sr_if *);
+void process_ip_packet(struct sr_instance  *sr,void *ip_packet, unsigned int len);
+void process_arp_packet(struct sr_instance *sr,void *arp_packet, unsigned int len);
 void sr_init(struct sr_instance* sr)
 {
     /* REQUIRES */
@@ -47,6 +50,7 @@ void sr_init(struct sr_instance* sr)
     pthread_create(&thread, &(sr->attr), sr_arpcache_timeout, sr);
     
     /* Add initialization code here! */
+
 
 } /* -- sr_init -- */
 
@@ -77,7 +81,7 @@ void sr_handlepacket(struct sr_instance* sr,
   assert(interface);
 
   printf("*** -> Received packet of length %d \n",len);
-
+  print_hdrs(packet,len);
   /* fill in code here */
   if(len<sizeof(sr_ethernet_hdr_t))
   {
@@ -85,28 +89,76 @@ void sr_handlepacket(struct sr_instance* sr,
     return;
   }
   sr_ethernet_hdr_t *curr_eth_frame=(sr_ethernet_hdr_t *)packet;
-  uint16_t eth_type = curr_eth_frame->ether_type;
-  printf("%d\n",eth_type);
+  uint16_t eth_type = ntohs(curr_eth_frame->ether_type);
   if(eth_type==ethertype_arp)
   {
     printf("receive arp");
-    process_arp_packet(sr,(void*)(packet+sizeof(sr_ethernet_hdr_t)),len-sizeof(sr_ethernet_hdr_t));
+    process_arp_packet(sr,(void*)packet,len);
   }
   else if (eth_type==ethertype_ip)
   {
     printf("receive ip packet");
-    process_ip_packet(sr,(void*)(packet+ sizeof(sr_ethernet_hdr_t)),len-sizeof(sr_ethernet_hdr_t));
+    process_ip_packet(sr,(void*)packet,len);
   }
   else 
     return;
 
 }/* end sr_ForwardPacket */
-void process_ip_packet(struct sr_instance  *sr,void *ip_datagram, unsigned int len)
+void process_ip_packet(struct sr_instance  *sr,void *ip_packet, unsigned int len)
 {
   return;
 }
-void process_arp_packet(struct sr_instance *sr,void *arp_datagram, unsigned int len)
+void process_arp_packet(struct sr_instance *sr,void *arp_packet, unsigned int len)
 {
+  sr_arp_hdr_t *curr_arp_hdr = (sr_arp_hdr_t *)(arp_packet+sizeof(sr_ethernet_hdr_t));
+  struct sr_if *if_frame_come_from = sr->if_list;
+  while(if_frame_come_from!=NULL)
+  {
+    /*printf("%d\n",curr_arp_hdr->ar_tip);
+    /sr_print_if(if_frame_come_from);*/
+    if ( curr_arp_hdr->ar_tip == if_frame_come_from->ip)
+    {
+      reply_arp(sr,arp_packet,len,if_frame_come_from);
+      break;
+    }
+    if_frame_come_from=if_frame_come_from->next;
+  }
+  /*free(curr_arp_hdr);
+  free(if_frame_come_from);*/
   return;
 }
+void reply_arp(struct sr_instance *sr, void *arp_packet,unsigned int len,struct sr_if *curr_sr_if)
+{
+  void *arp_frame_to_send=malloc(sizeof(sr_ethernet_hdr_t)+sizeof(sr_arp_hdr_t));
+  sr_ethernet_hdr_t *eth_header_to_send=(sr_ethernet_hdr_t *) malloc(sizeof(sr_ethernet_hdr_t));
+  memset((void*)eth_header_to_send,0,sizeof(sr_ethernet_hdr_t));
+  sr_arp_hdr_t *arp_header_to_send=(sr_arp_hdr_t *)malloc(sizeof(sr_arp_hdr_t));
+  memset((void*)arp_header_to_send,0,sizeof(sr_arp_hdr_t));
+  
 
+  /* set fields of ethernet header*/
+  memcpy((void*)(eth_header_to_send->ether_dhost) , (void *)(((sr_ethernet_hdr_t *)arp_packet)->ether_shost),ETHER_ADDR_LEN);
+  memcpy((void*)(eth_header_to_send->ether_shost) , (void *)(curr_sr_if->addr),ETHER_ADDR_LEN);
+  eth_header_to_send->ether_type=((sr_ethernet_hdr_t *)arp_packet)->ether_type;
+  /*set opcode = 2 as reply arp*/
+  arp_header_to_send->ar_hrd=((sr_arp_hdr_t *)(arp_packet+sizeof(sr_ethernet_hdr_t)))->ar_hrd;
+  arp_header_to_send->ar_pro=((sr_arp_hdr_t *)(arp_packet+sizeof(sr_ethernet_hdr_t)))->ar_pro;
+  arp_header_to_send->ar_hln=((sr_arp_hdr_t *)(arp_packet+sizeof(sr_ethernet_hdr_t)))->ar_hln;
+  arp_header_to_send->ar_pln=((sr_arp_hdr_t *)(arp_packet+sizeof(sr_ethernet_hdr_t)))->ar_pln;
+  arp_header_to_send->ar_op=htons(2);
+  /*set mac and ip of source and dest arp header*/
+  
+  memcpy((void *)(arp_header_to_send->ar_sha),(void *)(curr_sr_if->addr),ETHER_ADDR_LEN);
+  arp_header_to_send->ar_sip = curr_sr_if->ip;
+
+  memcpy((void *)(arp_header_to_send->ar_tha),(void *)(((sr_ethernet_hdr_t *)arp_packet)->ether_shost),ETHER_ADDR_LEN);
+  arp_header_to_send->ar_tip=((sr_arp_hdr_t *)(arp_packet+sizeof(sr_ethernet_hdr_t)))->ar_sip;
+
+  memcpy(arp_frame_to_send,(void *)eth_header_to_send,sizeof(sr_ethernet_hdr_t));
+  memcpy(arp_frame_to_send+sizeof(sr_ethernet_hdr_t),(void *)arp_header_to_send,sizeof(sr_arp_hdr_t));
+  print_hdrs(arp_frame_to_send,sizeof(sr_ethernet_hdr_t)+sizeof(sr_arp_hdr_t));
+
+  int ret_val= sr_send_packet(sr,(uint8_t *)arp_frame_to_send,len,curr_sr_if->name);
+  if(ret_val<0)
+    fprintf(stderr, "Failed to send\n");
+}
